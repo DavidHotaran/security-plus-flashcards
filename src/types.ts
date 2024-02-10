816c8a749a97a8e853b7cdf08a0297b240faad4a
David Hotaran <hotaran.d98@gmail.com>
2024-02-10 15:42:49 -0700

    new type for port component

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,10 @@
 export type DataObject = { key: string; value: string };
 export type DataObjectArray = DataObject[];
+export type PortObject = {
+  "unsecure protocol": string;
+  "original port": string;
+  "secure protocol": string;
+  "secure port": string;
+  notes: string;
+};
+export type PortObjectArray = PortObject[];
